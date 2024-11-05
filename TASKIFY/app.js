c779c286a4f67b1a1b7e1f9856cdd64ef3637a42
ismaaciil11 <mkwalaal1@gmail.com>
2024-11-05 13:39:59 +0300

    first commit

---
+++ TASKIFY/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const taskRoutes = require('.taskify/routes/taskRoutes');
+
+
+const HOSTNAME = 'localhost'
+const PORT = 9000
+
+
+const server = http.createServer((req, res) =>{
+    if (req.url.startsWith('/tasks')) {
+        taskRoutes(req, res)
+    } else {
+        res.writeHead(404, 'not found', {'content-type': 'application/json'})
+        res.end(JSON.stringify({
+            message: 'sorry, you got lost!' 
+        }))
+    }
+});
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`server running on port ${PORT}`)
+})
